e236c28fdfdc6efa7ae7dcb5f523640350b24065
Jonathan Tang <jonathanj.tang@mail.utoronto.ca>
2020-11-07 16:13:38 -0500

    Update unit tests to not depend on options settings

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -1,7 +1,6 @@
 import unittest
 from decimal import Decimal
 
-import options
 from drink import Drink
 from invalid_option_error import InvalidOptionError
 from pizza import Pizza
@@ -19,27 +18,57 @@ def test_pizza():
 class TestProduct(unittest.TestCase):
     def setUp(self):
         Product.set_type_to_price({"A": 0, "B": 1})
-        self.product = Product("A")
+        self.type_str = "A"
+        self.product = Product(self.type_str)
+
+    def test_set_type_to_price(self):
+        new_type_to_price = {"C", 2}
+        Product.set_type_to_price(new_type_to_price)
+        self.assertEqual(Product.type_to_price, new_type_to_price)
 
     def test_get_type(self):
-        self.assertEqual(self.product.get_type(), "A")
+        self.assertEqual(self.product.get_type(), self.type_str.upper())
 
     def test_set_invalid_type(self):
         self.assertRaises(InvalidOptionError, self.product.set_type, "Blabla")
-        self.assertEqual(self.product.type_, "A")
+        self.assertEqual(self.product.type_, self.type_str.upper())
 
     def test_get_price(self):
         self.assertRaises(NotImplementedError, self.product.get_price)
 
 
+class TestDrink(unittest.TestCase):
+    def setUp(self):
+        Drink.set_type_to_price({"COKE": Decimal("2.00"),
+                                 "JUICE": Decimal("3.01")})
+        self.drink = Drink("Juice")
+
+    def test_getprice(self):
+        self.assertEqual(self.drink.get_price(), Decimal("3.01"))
+
+
 class TestPizza(unittest.TestCase):
     def setUp(self):
-        Pizza.set_type_to_price(options.PIZZA_TYPE_TO_PRICE)
-        Pizza.set_size_to_price(options.PIZZA_SIZE_TO_PRICE)
-        Pizza.set_topping_to_price(options.PIZZA_TOPPING_TO_PRICE)
+        Pizza.set_type_to_price({"PEPPERONI"  : Decimal("6.99"),
+                                 "CUSTOM"     : Decimal("5.99")})
+        Pizza.set_size_to_price({"SMALL"      : Decimal("0.00"),
+                                 "MEDIUM"     : Decimal("2.00")})
+        Pizza.set_topping_to_price({"OLIVE"       : Decimal("1.00"),
+                                    "CHICKEN"     : Decimal("2.00"),
+                                    "BEEF"        : Decimal("2.00"),})
         self.pizza = Pizza("Small", ["Beef", "Chicken"], "Custom")
         self.prev_toppings_len = 2
 
+    def test_set_size_to_price(self):
+        new_size_to_price = {"Mini", 2}
+        Pizza.set_size_to_price(new_size_to_price)
+        self.assertEqual(Pizza.size_to_price, new_size_to_price)
+
+    def test_set_topping_to_price(self):
+        new_topping_to_price = {"Spinach", 4}
+        Pizza.set_topping_to_price(new_topping_to_price)
+        self.assertEqual(Pizza.topping_to_price, new_topping_to_price)
+    
     def test_initial_fields_and_setters(self):
         # Initialization uses all setters
         self.assertEqual(self.pizza.size, "SMALL")
@@ -83,18 +112,3 @@ class TestPizza(unittest.TestCase):
     def test_get_price(self):
         self.assertEqual(self.pizza.get_price(), Decimal("9.99"))
 
-
-class TestDrink(unittest.TestCase):
-    def setUp(self):
-        Drink.set_type_to_price(options.DRINK_TYPE_TO_PRICE)
-        self.drink = Drink("Juice")
-
-    def test_initial_fields_and_setters(self):
-        self.drink.set_type("Juice")
-        self.assertEqual(self.drink.type_, "JUICE")
-
-    def test_getters(self):
-        self.assertEqual(self.drink.get_type(), "JUICE")
-
-    def test_getprice(self):
-        self.assertEqual(self.drink.get_price(), Decimal("3.01"))
