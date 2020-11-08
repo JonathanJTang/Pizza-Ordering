0ebf6ab2692ee135f9fac7617e15486aa1bf9ffe
Jonathan Tang <jonathanj.tang@mail.utoronto.ca>
2020-11-07 23:21:02 -0500

    Begin work on PizzaParlour.py, including defining a data format the server receives

--- PizzaParlour.py
+++ PizzaParlour.py
@@ -1,10 +1,38 @@
-from flask import Flask
+from flask import Flask, request
+
+from cart import Cart
+from order import Order
 
 app = Flask("Assignment 2")
 
-@app.route('/pizza')
+orders = {}
+next_order_no = 1
+
+@app.route('/pizza', methods=["GET","POST"])
 def welcome_pizza():
     return 'Welcome to Pizza Planet!'
+
+def pizza_printer(pizza):
+    print("{}".format(pizza))
+
+@app.route('/api/orders', methods=["POST"])
+def create_order():
+    current_order_no = next_order_no
+    next_order_no += 1
+
+    try:
+        order_data = request.get_json()
+        print(order_data)
+    except:
+        pass  # just create an empty order?
+
+    new_cart = Cart()
+    orders[current_order_no] = Order(current_order_no, new_cart, None)
+    return current_order_no
+
+@app.route('/api/orders/<int:order_id>', methods=['GET'])
+def edit_order(order_id):
+    return order_id
 
 if __name__ == "__main__":
     app.run()
---
+++ data_formats.py
@@ -0,0 +1,36 @@
+# JSON data sample for Uber Eats
+{
+    "products": [
+        {"product_type": "pizza",
+         "size": "small",
+         "type": "custom",
+         "toppings": ["olive", "chicken"]
+        },
+        {"product_type": "drink",
+        "type": "coke"}
+    ],
+    "delivery_method": {
+        "delivery_type": "pickup",
+        "details": {}
+    }
+}
+
+order_schema = {
+    "$schema": 1,
+    "products": [
+        {"product_type": "pizza",
+         "size": "small",
+         "type": "custom",
+         "toppings": ["olive", "chicken"]
+        },
+        {"product_type": "drink",
+        "type": "coke"}
+    ],
+    "delivery_method": {
+        "delivery_type": "pickup",
+        "details": {}
+    }
+}
+
+# csv data for Foodora
+"ProductType,Proda, adjf, asdijfai"
