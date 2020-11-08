157a4f039e708d5293aa9e0b657ded2edc92c1f7
Jonathan Tang <jonathanj.tang@mail.utoronto.ca>
2020-11-08 00:43:44 -0500

    Update JSON schema

--- data_formats.py
+++ data_formats.py
@@ -1,13 +1,19 @@
 # JSON data sample for Uber Eats
 {
     "products": [
-        {"product_type": "pizza",
-         "size": "small",
-         "type": "custom",
-         "toppings": ["olive", "chicken"]
+        {
+            "product_category": "pizza",
+            "size": "small",
+            "type": "custom",
+            "toppings": [
+                "olive",
+                "chicken"
+            ]
         },
-        {"product_type": "drink",
-        "type": "coke"}
+        {
+            "product_category": "drink",
+            "type": "coke"
+        }
     ],
     "delivery_method": {
         "delivery_type": "pickup",
@@ -16,20 +22,119 @@
 }
 
 order_schema = {
-    "$schema": 1,
-    "products": [
-        {"product_type": "pizza",
-         "size": "small",
-         "type": "custom",
-         "toppings": ["olive", "chicken"]
+    "type": "object",
+    "properties": {
+        "products": {
+            "type": "array",
+            "items": {
+                "anyOf": [
+                    {
+                        "type": "object",
+                        "properties": {
+                            "product_category": {
+                                "type": "string",
+                                "pattern": "pizza"
+                            },
+                            "size": {
+                                "type": "string",
+                                "enum": [
+                                    "small",
+                                    "medium",
+                                    "large",
+                                    "extra_large"
+                                ]
+                            },
+                            "type": {
+                                "type": "string",
+                                "enum": [
+                                    "pepperoni",
+                                    "margherita",
+                                    "vegetarian",
+                                    "neapolitan",
+                                    "custom"
+                                ]
+                            },
+                            "toppings": {
+                                "type": "array",
+                                "items": {
+                                    "type": "string",
+                                    "enum": [
+                                        "olive",
+                                        "tomato",
+                                        "mushroom",
+                                        "jalapeno",
+                                        "chicken",
+                                        "beef",
+                                        "pepperoni",
+                                        "pineapple",
+                                        "bacon",
+                                        "cheese"
+                                    ]
+                                }
+                            }
+                        },
+                        "required": [
+                            "product_category",
+                            "size",
+                            "type",
+                            "toppings"
+                        ]
+                    },
+                    {
+                        "type": "object",
+                        "properties": {
+                            "product_category": {
+                                "type": "string",
+                                "pattern": "drink"
+                            },
+                            "type": {
+                                "type": "string",
+                                "enum": [
+                                    "coke",
+                                    "diet_coke",
+                                    "coke_zero",
+                                    "pepsi",
+                                    "diet_pepsi",
+                                    "dr_pepper",
+                                    "water",
+                                    "juice"
+                                ]
+                            },
+                        },
+                        "required": [
+                            "product_category",
+                            "type"
+                        ]
+                    }
+                ]
+            }
         },
-        {"product_type": "drink",
-        "type": "coke"}
-    ],
-    "delivery_method": {
-        "delivery_type": "pickup",
-        "details": {}
-    }
+        "delivery_method": {
+            "type": "object",
+            "properties": {
+                "delivery_type": {
+                    "type": "string",
+                    "enum": [
+                        "pickup",
+                        "pizzeria",
+                        "ubereats",
+                        "foodora"
+                    ]
+                },
+                "details": {
+                    "type": "object"
+                }
+            },
+            "required": [
+                "delivery_type",
+                "details"
+            ]
+        }
+    },
+    "required": [
+        "products",
+        "delivery_method"
+    ]
 }
 
 # csv data for Foodora
